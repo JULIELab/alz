82978ce28033b00e1bf59419ca3b8ca22f1a9cdc
t-duan <tinghui.duan@uni-jena.de>
2019-01-19 18:42:35 +0100

    clean

--- evaluation/internet_archive/ia.sh
+++
@@ -1,8 +0,0 @@
-tesseract allgemeineliter142unkngoog_0157.tif allgemeineliter142unkngoog_ts_1 -l deu &&
-tesseract allgemeineliter142unkngoog_0158.tif allgemeineliter142unkngoog_ts_2 -l deu &&
-tesseract allgemeineliter189unkngoog_0163.tif allgemeineliter189unkngoog_ts_1 -l deu &&
-tesseract allgemeineliter189unkngoog_0164.tif allgemeineliter189unkngoog_ts_2 -l deu &&
-tesseract allgemeineliter76unkngoog_0158.tif allgemeineliter76unkngoog_ts_1 -l deu &&
-tesseract allgemeineliter76unkngoog_0159.tif allgemeineliter76unkngoog_ts_2 -l deu &&
-tesseract gb_uaU9AAAAIAAJ_000155.png gb_uaU9AAAAIAAJ_ts_1 -l deu &&
-tesseract gb_uaU9AAAAIAAJ_000156.png gb_uaU9AAAAIAAJ_ts_2 -l deu
