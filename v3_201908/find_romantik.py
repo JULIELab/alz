7178eb45c44eeec4a2f3107d25bfc7872a06706c
Tinghui Duan <td@Tinghuis-MacBook-Air.local>
2019-09-17 19:43:16 +0200

    update

--- v3_201908/find_romantik.py
+++ v3_201908/find_romantik.py
@@ -5,16 +5,10 @@ from operator import itemgetter
 
 #romantik = re.compile(r'(Romant|[^a-zP]romant)')
 #romantik = re.compile(r'([Rr]omantiſ|[Rr]omant\.|[Rr]omantic|Romantik)')
-romantik = re.compile(r'[Rr]omanti[ſs]ch')
+romantik = re.compile(r'[Rr]omanti[fſs]ch')
 
-ro = False
 countyear = {}
-m=0
-line1 = ""
-line2 = ""
-line3 = ""
-line4 = ""
-line5 = ""
+context_n = 5
 
 os.chdir('xml_compiled/')
 filename = input("Output name:")
@@ -31,6 +25,7 @@ with open('../'+filename,'w',encoding="utf-8") as f:
 			for elem_ref in elem.findall("{http://www.tei-c.org/ns/1.0}ref"):
 				url = elem_ref.text
 			for elem_id in elem.findall("{http://www.tei-c.org/ns/1.0}id"):
+				window = []
 				n = 0
 				num = len(elem.findall("{http://www.tei-c.org/ns/1.0}content/{http://www.tei-c.org/ns/1.0}p"))
 				for elem_l in elem.findall("{http://www.tei-c.org/ns/1.0}content/{http://www.tei-c.org/ns/1.0}p"):
@@ -39,84 +34,78 @@ with open('../'+filename,'w',encoding="utf-8") as f:
 					ll = ll.replace('&gt;','>')
 					ll = ll.replace('&amp;','&')
 					ll = ll.replace('&lt;','<')
-					line1 = line2
-					line2 = line3
-					line3 = line4
-					line4 = line5
-					line5 = ll
-					output = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5
-					if ro:
-						if n + 1 == num:
-							ll = lastline + line4
-							lastline = ""
-							output = line2 + "\n" + line3 + "\n" + line4 + "\n" + line5
-						elif n == num:
-							ll = lastline + line5
-							output = line3 + "\n" + line4 + "\n" + line5
-							lastline = ""
+					lastline = ""
+					ro = False
+					if n <= context_n*2+1:
+						window.append(ll)
+						if n == 1:
+							continue
+						middle = int((len(window)-1)/2)
+						if n > 2:
+							lastline = window[middle-1]
+					elif n+context_n>num:
+						if len(window) % 2 == 1:
+							window = window[1:]
+							continue
+						elif len(window) == 2:
+							lastline = window[0]
+							window = window[1:]
+							middle = -int((len(window)+1)/2)
 						else:
-							ll = lastline + line3
-							lastline = ""
+							window = window[1:]
+							middle = -int((len(window)+1)/2)
+							lastline = window[middle-1]
 					else:
-						if n + 1 == num:
-							ll = line4
-							output = line2 + "\n" + line3 + "\n" + line4 + "\n" + line5
-						elif n == num:
-							ll = line5
-							output = line3 + "\n" + line4 + "\n" + line5
-						else:
-							ll = line3
-					if romantik.search(ll):
-						if n+1 == num:
-							n_l = n-1
-						elif n == num:
-							n_l = n
-						else:
-							n_l = n-2
-						treffer = [t,url,year,vol,elem_id.text,str(n_l),output]
-						treffer_list.append(treffer)
-						if not year in countyear:
-							countyear[year]=1
-						else:
-							countyear[year]+=1
-						if ro:
-							m+=1
-						# f.write(t)
-						# f.write('\n')
-						# f.write(url)
-						# f.write('\n')
-						# f.write(year)
-						# f.write('\t')
-						# f.write(vol)
-						# f.write('\n')
-						# f.write(elem_id.text)
-						# f.write('\t')
-						# f.write(str(n))
-						# f.write('\n')
-						# f.write(ll)
-						# f.write('\n')
-						# f.write('\n')
-					if line3.endswith('ro-') or line3.endswith('ro') or line3.endswith('Ro') or line3.endswith('Ro-') or line3.endswith('roman-') or line3.endswith('Roman-') or line3.endswith('roman') or line3.endswith('Roman'):
+						window = window[1:]
+						window.append(ll)
+						middle = int((len(window)-1)/2)
+						lastline = window[middle-1]
+					if lastline.endswith('ro-') or lastline.endswith('ro') or lastline.endswith('Ro') or lastline.endswith('Ro-') or lastline.endswith('roman-') or lastline.endswith('Roman-') or lastline.endswith('roman') or lastline.endswith('Roman'):
 						ro = True
-						lastline = line3.rstrip('-')
+						lastline = lastline.rstrip('-')
 					else:
 						ro = False
-	treffer_list = sorted(treffer_list, key = itemgetter(2))
-	for r in treffer_list:
-		f.write(r[0])
-		f.write('\n')
-		f.write(r[1])
-		f.write('\n')
-		f.write(r[2])
-		f.write('\t')
-		f.write(r[3])
-		f.write('\n')
-		f.write(r[4])
-		f.write('\t')
-		f.write(r[5])
-		f.write('\n')
-		f.write(r[6])
-		f.write('\n')
-		f.write('\n')
-print(countyear)
-print(m)
+					to_search = window[middle]
+					print(len(window))
+					if ro:
+						to_search = lastline+to_search
+					if romantik.search(to_search):
+						treffer = [t,url,year,vol,elem_id.text,str(n-abs(middle)),'\n'.join(window)]
+						with open("../belege/romantisch/"+t.split(".")[0]+"_"+elem_id.text+"_"+str(n-abs(middle))+".item","w",encoding="utf-8") as ff:
+							ff.write(treffer[0])
+							ff.write('\n')
+							ff.write(treffer[1])
+							ff.write('\n')
+							ff.write(treffer[2])
+							ff.write('\t')
+							ff.write(treffer[3])
+							ff.write('\n')
+							ff.write(treffer[4])
+							ff.write('\t')
+							ff.write(treffer[5])
+							ff.write('\n')
+							ff.write(treffer[6])
+							ff.write('\n')
+# 						treffer_list.append(treffer)
+# 						if not year in countyear:
+# 							countyear[year]=1
+# 						else:
+# 							countyear[year]+=1
+# 	treffer_list = sorted(treffer_list, key = itemgetter(2))
+# 	for r in treffer_list:
+# 		f.write(r[0])
+# 		f.write('\n')
+# 		f.write(r[1])
+# 		f.write('\n')
+# 		f.write(r[2])
+# 		f.write('\t')
+# 		f.write(r[3])
+# 		f.write('\n')
+# 		f.write(r[4])
+# 		f.write('\t')
+# 		f.write(r[5])
+# 		f.write('\n')
+# 		f.write(r[6])
+# 		f.write('\n')
+# 		f.write('\n')
+# print(countyear)
