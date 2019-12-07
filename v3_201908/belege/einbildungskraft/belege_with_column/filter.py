71c1ac99b2023bfbbccd8fe46a93f0b223c2ff2b
t-duan <10327873+t-duan@users.noreply.github.com>
2019-12-07 16:31:07 +0100

    update

--- v3_201908/belege/einbildungskraft/belege_with_column/filter.py
+++ v3_201908/belege/einbildungskraft/belege_with_column/filter.py
@@ -6,13 +6,14 @@ import pandas as pd
 
 #df.groupby('Column').count().to_csv("column_freq.tsv", sep='\t', encoding='utf-8')
 
-os.system("mkdir 05_ERBAUUNGSSCHRIFTEN")
+os.system("mkdir 06_ANDERE")
 
 os.chdir('all/')
 
 for beleg in os.listdir(os.getcwd()):
 	with open(beleg,"r", encoding="utf-8",errors='ignore') as f:
 		content = f.readlines()
-		#already processed: PHILOSOPHIE, SCHÖNE KÜNSTE, GESCHICHTE
-		if content[3].strip() == "ERBAUUNGSSCHRIFTEN":
-			os.system("cp "+beleg+" ../05_ERBAUUNGSSCHRIFTEN/")
+		#already processed: PHILOSOPHIE, SCHÖNE KÜNSTE, GESCHICHTE, ARZNEYGELAHRTHEIT, ERBAUUNGSSCHRIFTEN
+		if content[3].strip() not in ['PHILOSOPHIE','SCHÖNE KÜNSTE','ARZNEYGELAHRTHEIT','GESCHICHTE','ERBAUUNGSSCHRIFTEN','VERMISCHTE SCHRIFTEN','LITERARISCHE ANZEIGEN','LITERARISCHE NACHRICHTEN','KLEINE SCHRIFTEN','I. Ankündigung neuer Bücher','INTELLIGENZ DES BUCH- UND KUNSTHANDELS','Ankündigungen neuer Bücher','DEUTSCHLANDSJOURNALENLITERATUR']:
+			os.system("cp "+beleg+" ../06_ANDERE/")
+			print(content[3].strip())
